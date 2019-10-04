9ccf366206fdd60442ed3573510cb36aee435fdc
Brian Lauber <brian.lauber@33across.com>
2019-10-04 17:44:40 -0400

    AND / OR / NOT factories

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,42 @@
 
+export interface Expression<ARGS extends any[]> {
+  (...args: ARGS): boolean,
+}
+
+export function AND<ARGS extends any[]>(expressions: Expression<ARGS>[]): Expression<ARGS> {
+  function _and(this: any, ...args: ARGS): boolean {
+    for(const expr of expressions) {
+      if(!expr.apply(this, args)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  return _and;
+}
+
+
+export function OR<ARGS extends any[]>(expressions: Expression<ARGS>[]): Expression<ARGS> {
+  function _or(this: any, ...args: ARGS): boolean {
+    for(const expr of expressions) {
+      if(expr.apply(this, args)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  return _or;
+}
+
+export function NOT<ARGS extends any[]>(expr: Expression<ARGS>): Expression<ARGS> {
+  function _not(this: any, ...args: ARGS): boolean {
+    return !expr.apply(this, args);
+  }
+
+  return _not;
+}
 
